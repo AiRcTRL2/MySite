08a8e3e9a8f2137540b63ffccbeee4f941fedf7d
Karim <karim.elgendy2@mail.dcu.ie>
2019-07-02 15:54:37 +0100

    Module which uses Selenium to retrieve data from MSOFT

---
+++ py_modules/get_xbox.py
@@ -0,0 +1,52 @@
+from bs4 import BeautifulSoup
+import time
+from selenium import webdriver
+
+
+def get_xbox_details(game_name):
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
+
+    # initialize the driver
+    driver = webdriver.Chrome("../drivers/chromedriver.exe", options=options)
+
+    # go to default xbox one page
+    driver.get("https://www.xbox.com/en-GB/games/xbox-one?xr=shellnav")
+
+    # wait for results to load
+    time.sleep(3.5)
+
+    # define the search elements
+    xbox_search_field = driver.find_element_by_name('search-field')
+    xbox_search_button = driver.find_element_by_name('search-button')
+
+    # select input box
+    xbox_search_field.click()
+    # get search name from function arg
+    xbox_search_field.send_keys(game_name)
+
+    # perform search
+    xbox_search_button.click()
+
+    time.sleep(1.05)
+
+    # get all game div links for search results
+    xbox_search_results = driver.find_elements_by_class_name('gameDivLink')
+
+    # empty dict to hold results
+    game_results_parsed = {}
+
+    try:
+        # parse each result and find game data
+        for result in xbox_search_results:
+            soup = BeautifulSoup(result.get_attribute('innerHTML'), 'html.parser')
+            game_results_parsed[soup.find('h3').text] = [soup.find('img')['src'],
+                                                         soup.find('span', {"class": "textpricenew"}).text]
+        driver.close()
+
+    except AttributeError:
+        driver.close()
+        return 404
+
+    return game_results_parsed
